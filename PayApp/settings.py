a1f94ba9318ec1ab48e289209e742b4f46e708eb
AliHammad1 <alihammadshah050@gmail.com>
2024-06-03 19:46:25 +0500

    test

--- PayApp/settings.py
+++ PayApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fv!7(#44ym6$ua$*clknm(fbb#1zq_1qz5+h%p5#cb!1uq$e9u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://pay-app-ashen.vercel.app/','*']
 
 
 # Application definition
